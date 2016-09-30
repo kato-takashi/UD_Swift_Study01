e0bd095921bea72e7ab0c81dbd04d36cbbbc75c2
TK_Ratina <taka4.kato@gmail.com>
2016-09-30 09:39:46 +0900

    297. プログラムファイルをもう1つ作って画面へ設定する IBAction

--- SwiftButton1/SwiftButton1/NextViewController.swift
+++ SwiftButton1/SwiftButton1/NextViewController.swift
@@ -9,5 +9,22 @@
 import UIKit
 
 class NextViewController: UIViewController {
+    
+    @IBOutlet var changeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func change(sender: AnyObject) {
+        //次の画面
+        changeLabel.text = "暗号解除されました"
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
 
 }
