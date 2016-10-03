2d806e916dff48f0704106ca043011ca91cc9181
TK_Ratina <taka4.kato@gmail.com>
2016-10-04 08:54:49 +0900

    セクション25 303 画面に画像を設置する

---
+++ SwiftImageVIew/SwiftImageVIew/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftImageVIew
+//
+//  Created by KatoTakashi on 2016/10/04.
+//  Copyright © 2016年 KatoTakashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
