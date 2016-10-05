9b59544cb97bd41a63b502a97d99282b9a4286a8
TK_Ratina <taka4.kato@gmail.com>
2016-10-05 19:37:18 +0900

    セクション25 画像を扱うアプリケーション　swiftImageVIew

--- SwiftImageVIew/SwiftImageVIew/ViewController.swift
+++ SwiftImageVIew/SwiftImageVIew/ViewController.swift
@@ -10,14 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var backImageView: UIImageView!
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        count = 1
+    }
+    
+    
+    @IBAction func changeBtn(sender: AnyObject) {
+        
+        
+        if(count == 0){
+            //backImageViewのパーツ画像を変更
+            backImageView.image = UIImage(named: "back1.jpg")
+            count = 1
+        }else if(count == 1){
+            //backImageViewのパーツ画像を変更
+            backImageView.image = UIImage(named: "back2.jpg")
+            count = 0
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
