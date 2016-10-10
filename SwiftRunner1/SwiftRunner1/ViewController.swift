7cf0baab0fa50e248225375f4ea000f85c28635b
TK_Ratina <taka4.kato@gmail.com>
2016-10-11 08:51:31 +0900

    セクション26 タイマーの追懐方配列の使い方

--- SwiftRunner1/SwiftRunner1/ViewController.swift
+++ SwiftRunner1/SwiftRunner1/ViewController.swift
@@ -15,6 +15,15 @@ class ViewController: UIViewController {
     
     var imageArray = [UIImage]()
     
+    //経過時間用のタイマー
+    var timer1:NSTimer?
+    
+    //画像を配列から出すスピード
+    var timer2:NSTimer?
+    
+    var countUp = 0
+    var updateCount = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,13 +36,53 @@ class ViewController: UIViewController {
         let image5 = UIImage(named: "dash5.png")
         
         imageArray = [image0!, image1!, image2!, image3!, image4!, image5!]
+        
+        
+    
     }
 
     @IBAction func start(sender: AnyObject) {
+        updateCount = 0
+        countUp = 0
+        //経過時間用のタイマー
+        timer1 = NSTimer.scheduledTimerWithTimeInterval(
+            1.0,    //何秒ごとにこのメソッドを実行するか。
+            target: self,
+            selector: "keikajikan",  //どの関数を実行するか
+            userInfo: nil,
+            repeats: true    //繰り返し実行するかどうか
+        )
+        
+        timer2 = NSTimer.scheduledTimerWithTimeInterval(
+            0.1,    //何秒ごとにこのメソッドを実行するか。
+            target: self,
+            selector: "updateCount01",  //どの関数を実行するか
+            userInfo: nil,
+            repeats: true    //繰り返し実行するかどうか
+        )
     }
     
     
     @IBAction func stop(sender: AnyObject) {
+        timer1?.invalidate()
+        timer2?.invalidate()
+    }
+    
+    func keikajikan(){
+        countUp = countUp + 1
+        countLabel.text = String(countUp)
+    }
+    
+    func updateCount01(){
+        updateCount = updateCount + 1
+        
+        if(updateCount > 5){
+            updateCount = 0
+        }else{
+            runnerImageView.image = imageArray[updateCount]
+            
+        }
+            
     }
     
     override func didReceiveMemoryWarning() {
