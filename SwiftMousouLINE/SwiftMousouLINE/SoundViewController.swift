635edac0a464c745d6fd416961d86083506f88ae
TK_Ratina <taka4.kato@gmail.com>
2016-10-26 10:19:18 +0900

    セクション30 音声を再生する方法3 swift2.0

--- SwiftMousouLINE/SwiftMousouLINE/SoundViewController.swift
+++ SwiftMousouLINE/SwiftMousouLINE/SoundViewController.swift
@@ -7,17 +7,54 @@
 //
 
 import UIKit
+import AVFoundation
 
 class SoundViewController: UIViewController {
     
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet var imageView2: UIImageView!
+    var audioPlayer : AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //非表示
+        imageView2.hidden = true;
+        timeLabel.hidden = true;
+        
+        //着信音を再生
+        do {
+            // 音楽ファイルが"sample.mp3"の場合
+            let filePath = NSBundle.mainBundle().pathForResource("baby", ofType: "mp3")
+            let audioPath = NSURL(fileURLWithPath: filePath!)
+            audioPlayer = try AVAudioPlayer(contentsOfURL: audioPath)
+            audioPlayer.play()
+        } catch {
+            print("Error")
+        }
+        
+    }
+    
+    @IBAction func catchBtn(sender: AnyObject) {
+        print("catch")
+        //imageView2を表示へ
+        //        timerをスタート
+        //        タイムラベルへ秒数を表示
+        //        baby.mp3の音声を再生する
+    }
+    
+    
+    @IBAction func denyBtn(sender: AnyObject) {
+        print("deny")
+        
+        //電話を切る
+        //音声をストップ
+        //画面をトップへ戻す
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
     
